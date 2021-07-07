04d044cbd0e42e3d958e0a55e373a19bf562c030
dhivya <dhivyaramkamal@gmail.com>
2021-07-07 20:26:30 +0700

    React app created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import React, { Component } from 'react';
 import './App.css';
+import Header from './components/header'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>test</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
