9eb9b03f6aaf1d0e870ab7b73bd6b7dbc71971e4
dhivya <dhivyaramkamal@gmail.com>
2021-07-08 15:38:34 +0700

    Date selection enhancements

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ class App extends Component {
     let new_date = startDate;
     let dayMap = {"Sun": "Min", "Mon": "Sen", "Tue": "Sel", "Wed": "Rab", "Thu": "Kam", "Fri": "Jum", "Sat": "Sab"};
     let dayNames = {"Min": "Minggu", "Sen": "Senin", "Sel": "Selasa", "Rab": "Rabu", "Kam": "Kamis", "Jum": "Jumat", "Sab": "Sabtu"};
-   	this.state = {dates: []};
+   	let monthMap = {"Jan": "Januari", "Feb": "Februari", "Mar": "Maret", "Apr": "April", "May": "Mei", "Jun": "Juni", "Jul": "Juli", "Aug": "Agustus", "Sep": "September", "Oct": "Oktober", "Nov": "November", "Dec": "Desember"};
+    
+    this.state = {dates: []};
    	for(let i=0; i<14; i++)
   	{
   		if(i!==0)
@@ -23,11 +25,13 @@ class App extends Component {
       }
       
   	  let date = new_date.format('DD');
+      let year = new_date.format('YYYY');
+      let month = monthMap[new_date.format('MMM')];
   	  let day = dayMap[new_date.format('ddd')];
       let displayName = dayNames[day];
       let dateState = (day === "Sab" || day === "Min") ? "disabled" : "enabled";
       
-  	  this.state.dates[i] = {displayDate: date, displayDay: day, displayState: dateState,displayName: displayName};
+  	  this.state.dates[i] = {displayDate: date, displayDay: day, displayState: dateState,displayName: displayName, displayMonth: month, displayYear: year};
       startDate = new_date;
   	}
    	console.log(this.state.dates);
--- src/components/dateSelection.jsx
+++ src/components/dateSelection.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-
+import styles from '../css/dateSelection.css';
 const Button = styled.button`
   background: ${props => props.selected ? "gray" : "white"};
   color: ${props => props.selected ? "white" : "gray"};
@@ -10,24 +10,32 @@ const Button = styled.button`
   width: 50%;
 `
 const DateSelection = function(props) {
-  const[date, setDate] = useState(0);
+  let selectedDate = props.dates[0];
+  const[date, setDate] = useState(
+    {displayDate: selectedDate.displayDate,
+    displayName: selectedDate.displayName,
+    displayMonth: selectedDate.displayMonth,
+    displayYear: selectedDate.displayYear}
+  );
   const[mealTime, setmealTime] = useState("lunch");
     return (
     <div>
       <div className="dateContainer">        
-            {
-              props.dates.map(function(d, idx)
-              {
-                return (<div id= {"id_date_" + idx} className={"dateContent " + ((d.displayState === "disabled") ? "disabled" : "") + ((date === idx) ? "selected" : "")}  onClick={() => setDate(idx)}>
-                <span className="displayDay">{d.displayDay}</span><br></br>
-                <span className="displayDate">{d.displayDate}</span></div>)
-                setDate(0);
-              })
-            }
+        {
+          props.dates.map(function(d, idx)
+          {
+            return (<div id= {"id_date_" + idx} className={"dateContent " + ((d.displayState === "disabled") ? "disabled" : "") + ((date.displayDate === d.displayDate) ? "selected" : "")}  onClick={() => setDate({displayDate: d.displayDate, displayName: d.displayName, displayMonth: d.displayMonth, displayYear: d.displayYear})}>
+            <span className="displayDay">{d.displayDay}</span><br></br>
+            <span className="displayDate">{d.displayDate}</span></div>)
+          })
+        }
       </div>
       <div className="flexContainer">
           <Button selected = {(mealTime==="lunch") ? true : false} onClick={() => setmealTime("lunch")}>Lunch</Button>
           <Button selected = {(mealTime==="dinner") ? true : false} onClick={() => setmealTime("dinner")}>Dinner</Button>
+      </div>
+      <div className="selectedDate">
+        <p>{date.displayName}.{date.displayDate} {date.displayMonth} {date.displayYear}</p> 
       </div>
     </div>
     );
